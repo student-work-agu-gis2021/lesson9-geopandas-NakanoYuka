a23fff10ed982f8e4ece799e88c752741596b897
NakanoYuka <82096052+NakanoYuka@users.noreply.github.com>
2021-06-13 22:47:16 +0000

    I added comments to Problem3

--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -9,12 +9,14 @@
 # YOUR CODE HERE 1 to read data
 import geopandas as gpd
 from pyproj import CRS
+#read data
 data=gpd.read_file('Kruger_posts.shp')
 
 # - Check the crs of the input data. If this information is missing, set it as epsg:4326 (WGS84).
 # - Reproject the data from WGS84 to `EPSG:32735` -projection which stands for UTM Zone 35S (UTM zone for South Africa) to transform the data into metric system. (don't create a new variable, update the existing variable `data`!)"
 
 # YOUR CODE HERE 2 to set crs
+#Reproject the data from WGS84 to EPSG:32735
 data=data.to_crs(epsg=32735)
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -30,6 +32,7 @@ print(data.crs)
 #  - Group the data by userid
 
 #  YOUR CODE HERE 3 to group 
+#Group the data by userid
 grouped=data.groupby('userid')
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -44,6 +47,7 @@ assert len(grouped.groups) == data["userid"].nunique(), "Number of groups should
 # YOUR CODE HERE 4 to set movements
 import pandas as pd
 from shapely.geometry import LineString, Point
+#Convert movements into a GeoDataFrame
 movements=gpd.GeoDataFrame(columns=['userid','geometry'])
 # CODE FOR TESTING YOUR SOLUTION
 
